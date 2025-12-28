03425cc0889423609af7d6c2a3ed2fae93852f7a
Micheal Peter <michealpeter040@gmail.com>
2025-12-28 22:40:41 +0100

    feat: i am done

--- src/components/dashboard/admins/AdminsTable.tsx
+++ src/components/dashboard/admins/AdminsTable.tsx
@@ -97,7 +97,7 @@ export default function AdminsTable({
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Joined
               </th>
-              {user.role === 'superadmin' && (
+              {user?.role === 'superadmin' && (
                 <th className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Actions
                 </th>
